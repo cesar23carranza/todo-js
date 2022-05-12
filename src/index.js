8db200eca5113a861c8e57f280378a5895666c71
Cesar <cesar23carranza@gmail.com>
2022-05-12 14:37:30 -0500

    Primer commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-
-import { saludar } from './js/componentes.js';
 import './styles.css';
 
-const nombre = 'Victorino';
-
-saludar( nombre );
-
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
 
 
+
+export const todoList = new TodoList();
+
+// todoList.todos.forEach( todo => crearTodoHtml( todo ) );   Hace lo mismo que la de abajo
+
+todoList.todos.forEach( crearTodoHtml );
+
+// console.log('todos', todoList.todos );
